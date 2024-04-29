a4d91d596068178724da8d27d2ccce2270fc99d7
Hwei-Shin Harriman <hweishih@GS23545.SP.CS.CMU.EDU>
2024-04-29 19:55:37 -0400

    resized geometry symbols

--- src/core/geometryText.tsx
+++ src/core/geometryText.tsx
@@ -1,9 +1,9 @@
 export const congruent = (
-  <span className="text-3xl leading-4 font-extralight">{`\u2245`}</span>
+  <span className="text-xl leading-4 font-extralight">{`\u2245`}</span>
 );
 
 export const parallel = (
-  <span className="text-3xl leading-4 font-normal">{" \u2225 "}</span>
+  <span className="text-xl leading-4 font-normal">{" \u2225 "}</span>
 );
 
 export const comma = <span>{",\t"}</span>;
@@ -24,7 +24,7 @@ export const segmentStr = (s: string, clr?: string) => (
 // for static text
 export const triangleStr = (t: string) => (
   <span className="font-notoSerif">
-    <span className="text-l leading-4">{`\u25B5`}</span>
+    <span className="text-l leading-4 font-semibold">{`\u25B5`}</span>
     {t}
   </span>
 );
