c657d23f4ce3b896b78b271800b7cd4149e9d07a
RainyJin <rainyjin2016@gmail.com>
2024-05-31 10:14:11 -0400

    Added bottom margin for submit button

--- src/components/SubmitQuestion.tsx
+++ src/components/SubmitQuestion.tsx
@@ -18,7 +18,7 @@ export class SubmitQuestion extends React.Component<SubmitQuestionProps> {
 
   render() {
     return (
-      <div className="font-bold text-base mt-4 text-slate-50">
+      <div className="font-bold text-base mt-4 mb-4 text-slate-50">
         <button
           onClick={this.handleClick}
           className="bg-[#9459d4] hover:bg-[#7644ad] focus:bg-[#623691] py-1.5 px-2 rounded-md"
