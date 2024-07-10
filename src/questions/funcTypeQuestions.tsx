23f1d5b7f2c84e4fb6eb6e3c276d4bd942d40968
RainyJin <66048170+RainyJin@users.noreply.github.com>
2024-07-10 12:33:21 -0400

    Added scaffolding (#34)
    * Added scaffolding
    
    * removed console logs and added a hint button
    
    * updated reason to draw from reasons object, hide hint button when scaffolding
    
    ---------
    
    Co-authored-by: hsharriman <hwei.shin.harriman@gmail.com>

--- src/questions/funcTypeQuestions.tsx
+++ src/questions/funcTypeQuestions.tsx
@@ -1,10 +1,12 @@
 import { segmentQuestion, strs } from "../core/geometryText";
 import { Obj } from "../core/types/types";
+import { Reasons } from "../theorems/reasons";
 
 export interface Question {
   answerType: AnswerType;
   partlyScaffold?: string | JSX.Element;
   prompt: string | JSX.Element;
+  reason?: string;
   answers?: string[];
   type: QuestionType;
   id: string;
@@ -41,21 +43,22 @@ const diagramStateQuestion = (x: string, y: string, type: Obj) => {
 
 const id = (n: number) => `qID-${n}`;
 
-const mini = (reason: string) =>
-  `(Hint: click the row with ${reason} to check if there are any differences between the big construction and the mini one.)`;
-const relies = (reason: string) =>
-  `(Hint: click on the row with ${reason} to check if everything it relies on would still appear earlier in the proof.)`;
+const mini = (reason: string | JSX.Element) =>
+  `(Hint: Click the row with ${reason}. Do the diagrams match? Does it rely on the right information?)`;
+const relies = (reason: string | JSX.Element) =>
+  `(Hint: Click the row with ${reason}. In the proposed proof order, would the step rely only on steps that appear beforehand?)`;
 export const scaffolding = {
   mini,
   relies,
   diagram:
-    "(Hint: click the last row of the proof and check if they have the same tick marks.)",
+    "(Hint: Click the last row of the proof. Do the objects have the same number of ticks?)",
 };
 
 export const checkingProof1: Question[] = [
   {
     answerType: AnswerType.YesNo,
     prompt: "Is SAS triangle congruence correctly applied?",
+    reason: Reasons.SAS.title,
     type: QuestionType.Minifigures,
     id: id(1),
   },
@@ -77,6 +80,7 @@ export const checkingProof2: Question[] = [
   {
     answerType: AnswerType.YesNo,
     prompt: "Is Congruent Adjacent Angles correctly applied?",
+    reason: Reasons.CongAdjAngles.title,
     type: QuestionType.Minifigures,
     id: id(1),
   },
@@ -92,12 +96,14 @@ export const checkingProof3: Question[] = [
   {
     answerType: AnswerType.YesNo,
     prompt: "Is HL congruence correctly applied?",
+    reason: Reasons.HL.title,
     type: QuestionType.Minifigures,
     id: id(1),
   },
   {
     answerType: AnswerType.YesNo,
     prompt: "Is Def. Rectangle correctly applied?",
+    reason: Reasons.Rectangle.title,
     type: QuestionType.Minifigures,
     id: id(2),
   },
@@ -113,6 +119,7 @@ export const completeProof1: Question[] = [
   {
     answerType: AnswerType.YesNo,
     prompt: "Is Converse of Alternate Interior Angles correctly applied?",
+    reason: Reasons.ConverseAltInteriorAngs.title,
     type: QuestionType.Minifigures,
     id: id(1),
   },
@@ -120,6 +127,7 @@ export const completeProof1: Question[] = [
     answerType: AnswerType.YesNo,
     prompt:
       "Is there enough information to apply Vertical Angles between steps 1 and 2?",
+    reason: Reasons.VerticalAngles.title,
     type: QuestionType.ReliesOn,
     id: id(2),
   },
@@ -127,6 +135,7 @@ export const completeProof1: Question[] = [
     answerType: AnswerType.YesNo,
     prompt:
       "Is there enough information to apply SAS triangle congruence between steps 3 and 4?",
+    reason: Reasons.SAS.title,
     type: QuestionType.ReliesOn,
     id: id(3),
   },
@@ -136,12 +145,14 @@ export const completeProof2: Question[] = [
   {
     answerType: AnswerType.YesNo,
     prompt: "Is Def. Perpendicular Lines correctly applied?",
+    reason: Reasons.PerpendicularLines.title,
     type: QuestionType.Minifigures,
     id: id(1),
   },
   {
     answerType: AnswerType.YesNo,
     prompt: "Is Converse of Def. Midpoint correctly applied?",
+    reason: Reasons.ConverseMidpoint.title,
     type: QuestionType.Minifigures,
     id: id(2),
   },
@@ -149,6 +160,7 @@ export const completeProof2: Question[] = [
     answerType: AnswerType.YesNo,
     prompt:
       "Is there enough information to apply Congruent Adjacent Angles at step 4?",
+    reason: Reasons.CongAdjAngles.title,
     type: QuestionType.ReliesOn,
     id: id(3),
   },
@@ -156,6 +168,7 @@ export const completeProof2: Question[] = [
     answerType: AnswerType.YesNo,
     prompt:
       "Is there enough information to apply ASA triangle congruence at step 6?",
+    reason: Reasons.ASA.title,
     type: QuestionType.ReliesOn,
     id: id(4),
   },
@@ -165,6 +178,7 @@ export const incompleteProof2: Question[] = [
   {
     answerType: AnswerType.YesNo,
     prompt: "Is Vertical Angles correctly applied?",
+    reason: Reasons.VerticalAngles.title,
     type: QuestionType.Minifigures,
     id: id(1),
   },
@@ -172,6 +186,7 @@ export const incompleteProof2: Question[] = [
     answerType: AnswerType.YesNo,
     prompt:
       "Is there enough information to apply ASA triangle congruence between steps 2 and 3?",
+    reason: Reasons.ASA.title,
     type: QuestionType.ReliesOn,
     id: id(2),
   },
@@ -179,6 +194,7 @@ export const incompleteProof2: Question[] = [
     answerType: AnswerType.YesNo,
     prompt:
       "Is there enough information to apply Converse of Def. Midpoint between steps 4 and 5?",
+    reason: Reasons.ConverseMidpoint.title,
     type: QuestionType.ReliesOn,
     id: id(3),
   },
@@ -226,7 +242,7 @@ export const tutorial1Questions: Question[] = [
   {
     prompt:
       "Is there enough information to apply SAS Triangle Congruence between steps 2 and 3?",
-    type: QuestionType.DiagramState,
+    type: QuestionType.TutorialInstructions,
     id: id(2),
     answerType: AnswerType.YesNo,
   },
@@ -236,7 +252,7 @@ export const tutorial2Questions: Question[] = [
   {
     prompt: "Is SSS Triangle Congruence Correctly Applied?",
     answerType: AnswerType.YesNo,
-    type: QuestionType.DiagramState,
+    type: QuestionType.TutorialInstructions,
     id: id(1),
   },
 ];
